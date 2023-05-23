8cd0587078e18f2a8ce1e541a72bbe926ace8447
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2023-05-23 11:34:17 +0500

    Changes

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ const minimal_args = [
 // });
 
 // POST request handler
+/*
 app.post("/numbers", async (req, res) => {
   const url = "https://www.teacherspayteachers.com/";
   const outputPath = "example_screenshot.png";
@@ -159,6 +160,102 @@ async function scrapeData(label, page) {
   return { label: decodeURIComponent(label), resultNumber };
 }
 
+*/
+
+app.post("/numbers", async (req, res) => {
+  const url = "https://www.teacherspayteachers.com/";
+  const outputPath = "example_screenshot.png";
+  const results = [];
+  let labels = req.body;
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: minimal_args,
+    ignoreHTTPSErrors: true,
+    dumpio: false,
+  });
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on("request", (req) => {
+    if (
+      req.resourceType() === "image" ||
+      req.resourceType() === "stylesheet" ||
+      req.resourceType() === "font" ||
+      req.url().startsWith("https://cdn.siftscience.com/s.js") ||
+      req
+        .url()
+        .startsWith("https://cdn.heapanalytics.com/js/heap-3064244106.js") ||
+      req.url().startsWith("https://www.googletagmanager.com") ||
+      req.url().startsWith("https://sessions.bugsnag.com") ||
+      req.url().startsWith("https://a11000223989.cdn.optimizely.com") ||
+      req.url().startsWith("https://cdn.transcend.io") ||
+      req.url().startsWith("https://retail.googleapis.com") ||
+      req.url().startsWith("https://www.facebook.com") ||
+      req.url().startsWith("https://cdn3.optimizely.com") ||
+      req.url().startsWith("https://logx.optimizely.com") ||
+      req
+        .url()
+        .startsWith("https://www.teacherspayteachers.com/graph/graphql") ||
+      req.url().startsWith("https://tapi.optimizely.com") ||
+      req
+        .url()
+        .startsWith(
+          "https://static1.teacherspayteachers.com/tpt-frontend/optimizelyjs"
+        ) ||
+      req
+        .url()
+        .startsWith(
+          "https://suggest-production.teacherspayteachers.com/suggestions"
+        ) ||
+      req
+        .url()
+        .startsWith("https://www.teacherspayteachers.com/gateway/graphql") ||
+      req
+        .url()
+        .startsWith(
+          "https://static1.teacherspayteachers.com/tpt-frontend/releases/production/current/tpt-frontend.Drawer"
+        ) ||
+      req
+        .url()
+        .startsWith(
+          "https://static1.teacherspayteachers.com/tpt-frontend/releases/production/current/tpt-frontend"
+        ) ||
+      req.url().startsWith("https://cdn.attn.tv/") ||
+      req.url().startsWith("https://events.attentivemobile.com") ||
+      req.url().startsWith("https://www.teacherspayteachers.com/cdn-cgi/")
+    ) {
+      req.abort();
+    } else {
+      console.log(req.url());
+      req.continue();
+    }
+  });
+  console.log(labels);
+  labels = labels.map((element) => encodeURIComponent(element));
+  for (let i = 0; i < labels.length; i++) {
+    results.push(await scrapeData(labels[i], page));
+    if (results.length === labels.length) {
+      res.send(results);
+      await browser.close();
+    }
+  }
+});
+
+async function scrapeData(label, page) {
+  const data = [];
+  const LINK = "https://www.teacherspayteachers.com/Browse/Search:";
+  console.log(`Scraping ${label}`);
+
+  await page.goto(LINK + label, { waitUntil: "networkidle2" });
+  let html = await page.$eval(".ResultsForSearchResultHeader", (element) => {
+    return element.innerHTML;
+  });
+
+  var $ = Cheerio.load(html);
+  var result = $(".Text-module__noMarginBottom--VJdLv").text();
+  var resultNumber = result.split(" ")[0];
+  console.log(`Scraped ${label}`);
+  return { label: decodeURIComponent(label), resultNumber };
+}
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
